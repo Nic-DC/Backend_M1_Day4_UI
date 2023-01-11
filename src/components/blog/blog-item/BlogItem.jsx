70173292afda595fcdf5bacb7022f6a0ee1875dc
Nic <nic.dan.costea@gmail.com>
2023-01-11 16:59:59 +0100

    update

--- src/components/blog/blog-item/BlogItem.jsx
+++ src/components/blog/blog-item/BlogItem.jsx
@@ -1,3 +1,4 @@
+import { saveAs } from "file-saver";
 import React from "react";
 import { Card, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
@@ -5,13 +6,25 @@ import BlogAuthor from "../blog-author/BlogAuthor";
 import "./styles.css";
 const BlogItem = (props) => {
   const { title, cover, author, _id } = props;
+
+  const downloadPDF = async () => {
+    const url = process.env.REACT_APP_BE_URL;
+    const endpoint = `${url}/files/${id}/pdf`;
+
+    const res = await fetch(endpoint);
+    if (res.ok) {
+      const blob = await res.blob();
+      saveAs(blob, "blogPost.pdf");
+    }
+  };
+
   return (
     <Link to={`/blog/${_id}`} className="blog-link">
       <Card className="blog-card">
         <Card.Img variant="top" src={cover} className="blog-cover" />
         <Card.Body>
           <Card.Title>{title}</Card.Title>
-          <Button>Download PDF</Button>
+          <Button onClick={() => downloadPDF()}>Download PDF</Button>
         </Card.Body>
         <Card.Footer>
           <BlogAuthor {...author} />
